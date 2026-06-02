b428e5e4c096843a625ee96147bcb919fea87af5
asriths31 <asrithsai840@gmail.com>
2026-06-02 23:19:35 +0530

    changed the token storing

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:5000";
-// const baseURL = "https://agent-handler-server.onrender.com";
+// const baseURL = "http://localhost:5000";
+const baseURL = "https://agent-handler-server.onrender.com";
 
 
 const client = axios.create({
